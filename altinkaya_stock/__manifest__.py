ba114452d43da430d89effb8e0e9caca36c37049
onurugur <onurugur@gitlab.com>
2019-01-21 15:32:54 +0300

    refactoring module names

---
+++ altinkaya_stock/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Altinkaya Stock',
+    'version': '12.0',
+    'website': 'https://www.altinkaya.com.tr',
+    'author':'Ahmet Altınışık,OnurUgur,Codequarters',
+    'category': 'Stock',
+    'summary': 'add specialized stock extensions',
+    'description': """
+     """,
+    'depends': [
+        'stock'
+    ],
+    'data': [
+            'views/stock_picking_views.xml',
+            'views/stock_view.xml',
+    ],
+    'installable' : True,
+    'auto_install' : False,
+}
