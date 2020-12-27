de2d1e3d9a1de37900d43d8983ba94c10fd30de3
Doğan ALTUNBAY <dogan.altunbay@codequarters.com>
2020-12-27 14:26:36 +0300

    e-invoicing code review

--- l10n_tr_base/__manifest__.py
+++ l10n_tr_base/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name' : 'Turkey Base Extensions',
-    'version' : '12.0',
-    'author' : 'CODEQUARTERS',
+    'name': 'Turkey Base Extensions',
+    'version': '12.0',
+    'author': 'CODEQUARTERS',
     'summary': 'Includes Turkey Base Extensions',
     'description': """
 """,
     'website': 'https://www.codequarters.com',
-    'depends' : [
+    'depends': [
         "base",
         ],
     'data': [
         'views/res_partner_views.xml',
+        'views/res_company_views.xml'
     ],
     'installable': True,
     'auto_install': False,
