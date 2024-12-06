b4ad31e14fedbcd238c8b8682cd8c4775cbc7cef
Ismail Cagan Yilmaz <ismail.cagan.yilmaz@gmail.com>
2024-12-06 14:39:15 +0300

    Added website_catch_500
    [16.0][ADD] Created website_catch_500
    
    [16.0][ADD] Created website_catch_500

---
+++ website_catch_500/models/ir_http.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Ismail Cagan Yilmaz (https://github.com/milleniumkid)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import models
+from odoo.http import request
+
+
+class HttpInherit(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _get_error_html(cls, env, code, values):
+        if code == 500:
+        # if code == 500 or code == "page_500"
+            website = request.website
+            if website and website.catch_500_errors:
+                website._catch_500_error(request.httprequest)
+        return super()._get_error_html(env, code, values)
