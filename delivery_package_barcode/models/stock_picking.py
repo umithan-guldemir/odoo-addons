813915af8b06c6f53e79e90322cc0e7c009961a0
yibudak <yibudak@gmail.com>
2025-01-06 09:35:13 +0000

    [FIX] All files are refactored in OCA standards

--- delivery_package_barcode/models/stock_picking.py
+++ delivery_package_barcode/models/stock_picking.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
