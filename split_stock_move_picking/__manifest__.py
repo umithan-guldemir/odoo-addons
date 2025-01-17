03a794f2a8cd7b0d2b5e5aa4d31795540e152be8
Ismail Cagan Yilmaz <ismail.cagan.yilmaz@gmail.com>
2025-01-17 10:47:43 +0300

    [MIG] split_stock_move_picking: Migration to 16.0

--- split_stock_move_picking/__manifest__.py
+++ split_stock_move_picking/__manifest__.py
@@ -1,27 +1,20 @@
-# -*- coding: utf-8 -*-
-{
-    'name': 'Split Stock Move Picking',
+# Copyright 2025 Ismail Cagan Yilmaz (https://github.com/milleniumkid)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-    'summary': """
+{
+    "name": "Split Stock Move Picking",
+    "summary": """
        Splits stock.move record in pickings.
        """,
-
-    'description': """
-       Splits stock.move record in stock.picking model.
-    """,
-
-    'author': "yibudak",
-
-    'website': "https://github.com/yibudak",
-
-    'category': 'Product',
-
-    'version': '12.0.0.1',
-
-    'depends': ['base', 'stock'],
-
-    'data': [
-        'wizard/wizard_split_picking_line.xml',
-        'views/stock_picking_view.xml',
+    "author": "yibudak, Altinkaya Enclosures",
+    "website": "https://github.com/altinkaya-opensource/odoo-addons",
+    "category": "Product",
+    "license": "LGPL-3",
+    "version": "16.0.1.0.0",
+    "depends": ["base", "stock"],
+    "data": [
+        "wizard/wizard_split_picking_line.xml",
+        "views/stock_picking_view.xml",
+        "security/ir.model.access.csv",
     ],
 }
--- split_stock_move_picking/wizard/__init__.py
+++ split_stock_move_picking/wizard/__init__.py
@@ -1,3 +1,4 @@
-# -*- coding: utf-8 -*-
+# Copyright 2025 Ismail Cagan Yilmaz (https://github.com/milleniumkid)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import split_stock_move_picking
