694bed7527680e4271bde0cc7739b3128c2b6f0b
yibudak <yigit@altinkaya.com>
2025-01-29 13:39:09 +0000

    [FIX] total_balance computing optimized

--- altinkaya_account/models/account_move.py
+++ altinkaya_account/models/account_move.py
@@ -7,29 +7,14 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    total_balance = fields.Float(readonly=True)
+    total_balance = fields.Float(compute="_compute_partner_balance", store=False)
 
-    @api.depends("partner_id")
-    def partner_balance(self, partner_id):
-        partner = self.env["res.partner"].browse(partner_id)
-        if partner.commercial_partner_id != partner:
-            comm_partner = partner.commercial_partner_id
-            balance = comm_partner.credit - comm_partner.debit
-        else:
-            balance = partner.credit - partner.debit
-        return balance
-
-    @api.model
-    def create(self, vals_list):
-        if vals_list.get("partner_id"):
-            vals_list["total_balance"] = self.partner_balance(vals_list["partner_id"])
-        return super().create(vals_list)
-
-    def write(self, vals):
-        for invoice in self:
-            if vals.get("partner_id"):
-                partner_id = vals["partner_id"]
+    def _compute_partner_balance(self):
+        for move in self:
+            partner = move.partner_id
+            if partner.commercial_partner_id != partner:
+                comm_partner = partner.commercial_partner_id
+                balance = comm_partner.credit - comm_partner.debit
             else:
-                partner_id = invoice.partner_id.id
-            vals["total_balance"] = self.partner_balance(partner_id)
-        return super().write(vals)
+                balance = partner.credit - partner.debit
+            move.total_balance = balance
